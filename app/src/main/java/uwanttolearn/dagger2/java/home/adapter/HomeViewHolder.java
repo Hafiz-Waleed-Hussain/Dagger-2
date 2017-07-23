2cdf839f71dbaa4e9b92e7d027270603e00cf968
Hafiz Waleed Hussain <hafiz.waleed@icarasia.com>
2017-07-23 14:35:57 +0800

    Constructor injection

--- app/src/main/java/uwanttolearn/dagger2/java/home/adapter/HomeViewHolder.java
+++ app/src/main/java/uwanttolearn/dagger2/java/home/adapter/HomeViewHolder.java
@@ -11,6 +11,8 @@ import io.reactivex.Observer;
 import uwanttolearn.dagger2.R;
 import uwanttolearn.dagger2.java.pojos.GitHubUser;
 
+import static com.bumptech.glide.Glide.with;
+
 /**
  * Created by waleed on 22/07/2017.
  */
@@ -19,25 +21,24 @@ final class HomeViewHolder extends RecyclerView.ViewHolder {
 
     private final View parentView;
     private final Observer<GitHubUser> clickObserrver;
+    private final Glide glide;
     private final TextView nameTextView;
     private final ImageView picImageView;
 
-    public HomeViewHolder(View itemView, Observer<GitHubUser> clickObserver) {
+    public HomeViewHolder(View itemView, Observer<GitHubUser> clickObserver, Glide glide) {
         super(itemView);
         nameTextView = (TextView) itemView.findViewById(R.id.RowHome_name_text_view);
         picImageView = (ImageView) itemView.findViewById(R.id.RowHome_user_image_view);
         parentView = itemView;
         this.clickObserrver = clickObserver;
+        this.glide = glide;
     }
 
     public void bind(GitHubUser gitHubUser) {
-
-        Glide.with(picImageView.getContext())
+        glide.with(picImageView.getContext())
                 .load(gitHubUser.getAvatarUrl())
                 .into(picImageView);
         nameTextView.setText(gitHubUser.getLogin());
         parentView.setOnClickListener(v -> clickObserrver.onNext(gitHubUser));
-
-
     }
 }
